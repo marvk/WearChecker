e437a6f639da66f07a2a4ef19af3f42214a99ea4
Marvin K <marvk3@gmail.com>
2015-01-30 03:13:26 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | UnsupportedLookAndFeelException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        new Controller();
+    }
+}
